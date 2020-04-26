86cb39ae76acf1ecb95cafdbfd8bde839288e5db
tom <devbytom@gmail.com>
2020-04-26 17:55:12 -0300

    nginx + certbot

---
+++ droplet/certbot.sh
@@ -0,0 +1,2 @@
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt install python-certbot-nginx
--- droplet/setup.sh
+++ droplet/setup.sh
@@ -1,8 +1,50 @@
 sudo apt update
 
+sudo apt install nginx
+
+echo "
+  worker_processes  1;
+  events {
+      worker_connections  1024;
+  }
+
+  http {
+      include       mime.types;
+      default_type  application/octet-stream;
+
+      sendfile        on;
+      keepalive_timeout  65;
+
+      upstream backend {
+          server localhost:3000;
+      }
+
+      server {
+          server_name echotom.dev;
+          listen 443 ssl;
+          ssl_certificate /etc/letsencrypt/live/echotom.dev/cert.pem;
+          ssl_certificate_key /etc/letsencrypt/live/echotom.dev/privkey.pem;
+          ssl_verify_client off;
+          location / {
+              proxy_pass  https://backend;
+              proxy_set_header Host $http_host;
+              proxy_set_header X_FORWARDED_PROTO https;
+          }
+      }
+  }
+" >> /etc/nginx/nginx.confg
+
+sudo systemctl start nginx
+stemctl status nginx
+
 sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
 docker build -t twitter-api /home/app/
 docker run -d -p 80:3000 twitter-api:latest
+
+
+
+/etc/nginx/conf.d/
+/etc/nginx/sites-enabled/
